111942b87b3bc8b7c7e22add15d606a0b4f3928d
lilDrochilo <115777990+lilDrochilo@users.noreply.github.com>
2023-06-15 22:49:03 +0700

    Update Mix.cs

--- AvaloniaApplicationPracticeOne/Models/Mix.cs
+++ AvaloniaApplicationPracticeOne/Models/Mix.cs
@@ -21,7 +21,7 @@ public partial class Mix : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=10.30.0.137;Port=5432;Database=saneyaskin;Username=saneyaskin;Password=1Z4X1c5ZxC");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=55285;Database=Mix;Username=Mix;Password=0409");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
